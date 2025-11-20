9a8716564a2e999c0f0e39e6838909d1edfd79ec
GitBchan <chan4527@naver.com>
2025-11-20 23:27:32 +0900

    spring setting

---
+++ src/main/java/org/tradinggate/backend/TradingGateBackendApplication.java
@@ -0,0 +1,13 @@
+package org.tradinggate.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingGateBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingGateBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tradinggate/backend/TradingGateBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tradinggate.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradingGateBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
