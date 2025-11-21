f39dcbee2b131db7929f5d4eb254e0927b31b6b7
GitBchan <chan4527@naver.com>
2025-11-21 12:11:49 +0900

    infra settting

--- src/main/java/org/tradinggate/backend/TradingGateBackendApplication.java
+++ src/main/java/org/tradinggate/backend/TradingGateBackendApplication.java
@@ -1,5 +1,6 @@
 package org.tradinggate.backend;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TradingGateBackendApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.load();
+        System.setProperty("spring.profiles.active", dotenv.get("spring.profiles.active"));
         SpringApplication.run(TradingGateBackendApplication.class, args);
     }
-
 }
